74713691cbf97ec06b1a3370c0fdb73bccff36b4
Rin <64449865+lee-haell@users.noreply.github.com>
2025-05-22 20:20:49 +0900

    [Edit] classList.contains, .remove, .add

--- app.js
+++ app.js
@@ -165,10 +165,11 @@ const title3 = document.querySelector(".hello h1");
 //css에 active 클래스 추가 (html 스타일 추가)
 //javascript에 함수와 클릭 이벤트 추가 (html 제어)
 function titleClick(){
-    if(title3.className === "active"){
-        title3.className = "";
+    const activeTitle = "active";
+    if(title3.classList.contains(activeTitle)){
+        title3.classList.remove(activeTitle);
     } else {
-        title3.className = "active";
+        title3.classList.add(activeTitle);
     }
 }
 
