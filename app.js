7e30590e2da51fb646c6b72616587cba7c07e038
Rin <64449865+lee-haell@users.noreply.github.com>
2025-05-23 20:14:30 +0900

    [Edit] form submission

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const loginInput = document.querySelector("#login-form input");
 const loginBtn = document.querySelector("#login-form button");
 
 function onLoginBtnClick(){
-    console.log(loginInput.value);
+    const userName = loginInput.value;
+    if(userName === ""){
+        alert("Please write your name!");
+    } else if(userName.length > 15){
+        alert("Your name is too long~");
+    }
 }
 
 loginBtn.addEventListener("click", onLoginBtnClick);
