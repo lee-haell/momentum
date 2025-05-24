1ef9d4192071f79c31d3e6a42a81f39b0ca42b43
Rin <64449865+lee-haell@users.noreply.github.com>
2025-05-24 19:34:42 +0900

    [Edit] 설명 주석 추가

--- app.js
+++ app.js
@@ -2,26 +2,33 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#userNameInput");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden"; //일반적으로 string만 포함된 변수는 대문자로 표기, string 저장하고 싶을 때 사용
-const USERNAME_KEY = "username";
-function onLoginSubmit(evnet){
-    evnet.preventDefault(); //input의 submit을 막는 역할
 
-    const userName = loginInput.value;
+//일반적으로 string만 포함된 변수는 대문자로 표기, string 저장하고 싶을 때 사용
+const HIDDEN_CLASSNAME = "hidden";
+//local storage의 key값 
+const USERNAME_KEY = "username";
+
+
+function onLoginSubmit(evnet){
+    //input의 submit을 막는 역할, submit 이벤트에서 무조건 맨 앞에 써야 함
+    evnet.preventDefault();
+
+    const userValue = loginInput.value;
 
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    console.log(userName);
     
-    localStorage.setItem(USERNAME_KEY, userName);
-    // greeting.innerText = "Hello, " + userName;
-    paintGreeting(userName);
+    localStorage.setItem(USERNAME_KEY, userValue);
+    paintGreeting(userValue);
 }
+
 
 function paintGreeting(username){
     greeting.innerText = `Hello, ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+
+//순서중요) savedUserName을 먼저 가져와야, form을 보여줄지/greeting을 보여줄지 선택 가능
 const savedUserName = localStorage.getItem(USERNAME_KEY);
 
 if(savedUserName === null){
