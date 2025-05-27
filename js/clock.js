cef2586b3281270fecc80bed646dc6e4251d13e8
Rin <64449865+lee-haell@users.noreply.github.com>
2025-05-27 22:04:42 +0900

    [Edit] getClock function > hours, minutes, seconds const 생성, string으로 변경, padStrat 추가가

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,11 @@
 const clock = document.querySelector("#clock");
 
 function getClock(){
-    const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const date = new Date(); //호출하는 당시의 현재 날짜와 시간을 알려줌
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock(); //시계 바로 호출
