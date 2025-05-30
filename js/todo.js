9a2afc4ba272d1a2018f7b05b0d30a90df2e6595
Rin <64449865+lee-haell@users.noreply.github.com>
2025-05-31 00:30:35 +0900

    [Add] deleting ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,14 +2,26 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+//todo list를 지우는 함수
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+//todolist에 html element 추가
 function paintToDo(newToDo){
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
     li.appendChild(span);
+    li.appendChild(button);
     span.innerText = newToDo;
     toDoList.appendChild(li);
 }
 
+//todo input에 값을 입력할 때 함수
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
